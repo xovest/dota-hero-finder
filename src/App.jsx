60cd4f52d4a6d771d817e48a268332bb0be308d0
xovest <79008917+xovest@users.noreply.github.com>
2022-03-05 09:51:18 +0200

    im really bad

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 import img from './heroes/aba.png'
 import img2 from './heroes/alc.png'
@@ -141,15 +142,20 @@ function App() {
     return array;
   }  
 
-  const arr = [img, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20, img21, img22, img23, img24, img25, img26, img27, img28, img29, img30, img31, img32, img33, img34, img35, img36, img37, img38, img39, img40, img41, img42, img43, img44, img45, img46, img47, img48, img49, img50, img51, img52, img53, img54, img55, img56, img57, img58, img59, img60, img61, img62, img63, img64, img65, img66, img67, img68, img69, img70, img71, img72, img73, img74, img75, img76, img77, img78, img79, img80, img81, img82, img83, img84, img85, img86, img87, img88, img89, img90, img91, img92, img93, img94, img95, img96, img97, img98, img99, img100, img101, img102, img103, img104, img105, img106, img107, img108, img109, img110, img111, img112, img113, img114, img115, img116, img117, img118, img119, img120, img121, img122]
+  let arr = [img, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20, img21, img22, img23, img24, img25, img26, img27, img28, img29, img30, img31, img32, img33, img34, img35, img36, img37, img38, img39, img40, img41, img42, img43, img44, img45, img46, img47, img48, img49, img50, img51, img52, img53, img54, img55, img56, img57, img58, img59, img60, img61, img62, img63, img64, img65, img66, img67, img68, img69, img70, img71, img72, img73, img74, img75, img76, img77, img78, img79, img80, img81, img82, img83, img84, img85, img86, img87, img88, img89, img90, img91, img92, img93, img94, img95, img96, img97, img98, img99, img100, img101, img102, img103, img104, img105, img106, img107, img108, img109, img110, img111, img112, img113, img114, img115, img116, img117, img118, img119, img120, img121, img122]
   shuffle(arr)
+  arr = arr.slice(0, 70)
 
-  arr.slice(0, 70)
+  let item1 = arr[Math.floor(Math.random()*arr.length)];
+  let item2 = arr[Math.floor(Math.random()*arr.length)];
+  let item3 = arr[Math.floor(Math.random()*arr.length)];
 
-  var item1 = arr[Math.floor(Math.random()*arr.length)];
-  var item2 = arr[Math.floor(Math.random()*arr.length)];
-  var item3 = arr[Math.floor(Math.random()*arr.length)];
+  const [fade, setFade] = useState(true)
 
+  function handleClick(e) {
+    e.preventDefault();
+    setFade(false)
+  }
 
   return (
     <div className='card'>
@@ -161,8 +167,8 @@ function App() {
       </div>
       <div className='striped-border'></div>
       <div className='grid'>
-        {arr.slice(0, 70).map((hero, index) => (
-          <img src={hero} alt="asd" key={index} />
+        {arr.map((hero, index) => (
+          <img src={hero} alt="asd" key={index} className={`fade ${fade}`} onClick={e => handleClick(e)} />
         ))}
       </div>
     </div>
